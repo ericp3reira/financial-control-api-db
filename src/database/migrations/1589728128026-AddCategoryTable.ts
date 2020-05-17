5dc529a2aaacfdaec71dbf8cae3a58470862293a
Eric Pereira <ericmp@gmail.com>
2020-05-17 18:45:44 +0100

    Create Transactions migration

--- src/database/migrations/1589728128026-AddCategoryTable.ts
+++ src/database/migrations/1589728128026-AddCategoryTable.ts
@@ -9,7 +9,7 @@ export default class AddCategoryTable1589728128026
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -1,16 +1,39 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  JoinColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import Category from './Category';
+
+@Entity('transactions')
 class Transaction {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column('varchar')
   title: string;
 
+  @Column('varchar')
   type: 'income' | 'outcome';
 
+  @Column('numeric')
   value: number;
 
+  @Column('uuid')
   category_id: string;
 
+  @ManyToOne(() => Category)
+  @JoinColumn({ name: 'category_id' })
+  category: Category;
+
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
